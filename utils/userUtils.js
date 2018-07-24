1c225bd07bae3b996f7aaf9d222cf575bba9e3ca
Muthomi Mate <muthomimate@gmail.com>
2018-07-24 16:42:06 +0300

    ft(login):add login functionality and modify folder structure

--- utils/userUtils.js
+++ utils/userUtils.js
@@ -1,20 +1,29 @@
-validateEmail = (email) =>{
+const User = require("../models/user")
+const {InvalidParamsError} = require("./errorHandlers")
+validateEmail = (email, res) =>{
     const  emailPattern =  /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/
     if(!emailPattern.test(email)){
-        throw new Error("Invalid Email address")
+        const error = new InvalidParamsError("Invalid Email address")
+        res.json({error: error.message})
     }
     return email
 }
 
- validatePasswordLength = (password) => {
+ validatePasswordLength = (password, res) => {
     const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/
     if(!passwordRegex.test(password)){
-        throw new Error("The password must be 8 characters long and must contain small, uppercase, digits and special characters")
+        const error  = new InvalidParamsError("The password must be 8 characters long and must contain small, uppercase, digits and special characters")
+        res.json({error: error.message})
     }
 
+ }
+ checkUserExists = (email) = () =>{
+    const userExists =  User.findOne({email})
+    return userExists
  }
 
 module.exports = {
     validateEmail,
-    validatePasswordLength
+    validatePasswordLength,
+    checkUserExists
 }
