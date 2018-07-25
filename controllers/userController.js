26d4b401bed1965863dcc094f6cbde22bf04a751
Muthomi Mate <muthomimate@gmail.com>
2018-07-25 19:37:29 +0300

    ft(jwt):add json web tokens on login

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,7 @@ class UserController{
 
     static async registerUser(req, res){
         try {
-            await registerUser(req,res)
+             await registerUser(req,res)
             res.json({})
         } catch(error){
             res.json(error.message);
@@ -16,8 +16,11 @@ class UserController{
 
     static async LoginUser(req, res){
         try{
-            await loginUser(req)
-            res.json({res:"Logged in successfully"})
+            const user = await loginUser(req)
+            res.json({
+                res:"Logged in successfully",
+                token: user
+            })
         }catch(error){
            res.json({ error:errorHandler(error)})
         }
