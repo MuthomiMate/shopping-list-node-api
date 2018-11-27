c4b4ae21d1836216c21f754a2312db45b7add36a
Muthomi <muthomimate@gmail.com>
2018-11-27 12:35:09 +0300

    chore(tests):update failing tests

--- routes.js
+++ routes.js
@@ -9,9 +9,9 @@ router.use(bodyParser.json());
 
 const UserController = require('./controllers/userController');
 
+router.post('/Login', UserController.loginUser)
 router.post('/register', UserController.registerUser);
-router.post('/login', UserController.loginUser)
-router.get('/', (req, res) =>{
+router.get('/login', (req, res) =>{
     res.json({response: 'Welcome to the node api entrypoint'});
 })
 module.exports = router;
